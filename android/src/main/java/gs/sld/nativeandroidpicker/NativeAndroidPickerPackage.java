b7e8f9022fa8bbd07ccda73e8704eae57fc8e4cf
Jan Tovarnak <jan@sld.gs>
2018-03-05 16:25:40 +0100

    package fix

--- android/src/main/java/gs/sld/nativeandroidpicker/NativeAndroidPickerPackage.java
+++ android/src/main/java/gs/sld/nativeandroidpicker/NativeAndroidPickerPackage.java
@@ -12,7 +12,7 @@ import com.facebook.react.uimanager.ViewManager;
 
 public class NativeAndroidPickerPackage implements ReactPackage {
 
-    public NativedAndroidPickerPackage() {}
+    public NativeAndroidPickerPackage() {}
     public NativeAndroidPickerPackage(Activity activity) {}
 
     @Override
